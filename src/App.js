49c2d03a4b7f434b86cd6a9125b81e1898c600de
beucherie remi <beucherie.r@gmail.com>
2022-04-20 14:58:15 +0200

    make it better

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Card from './components/Card';
+
+const App = () => {
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("")
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch).then((res) => setMealsData(res.data.meals))
+  }, [inputSearch])
+  return (
+    <div className="app-container">
+      <h1>React Cooking App</h1>
+      <input type="text" placeholder="Choose an ingredient" onChange={(e) => setInputSearch(e.target.value)} />
+      <div className="meals-container">
+        {mealsData && mealsData.slice(0, 24).map((meal) => (
+          <Card key={meal.idMeal} meal={meal} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
